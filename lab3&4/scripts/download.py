6a8e0e9699e659955dd31a94e6269d2a09d8874c
stasiek <janikstasiek@gmail.com>
2023-03-23 11:34:13 +0100

    Finished download.py and plots.py from lab 3.

---
+++ lab3&4/scripts/download.py
@@ -0,0 +1,99 @@
+import os
+
+import requests as r
+from bs4 import BeautifulSoup
+from random import shuffle
+import wget
+from tqdm import tqdm
+from argparse import ArgumentParser
+
+
+def download_kingdom(king: str, path: str, count: int = 100):
+    ftp_url = f"https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/{king}/"
+
+    html_page = r.get(ftp_url).text
+    soup = BeautifulSoup(html_page, 'html.parser')
+    proteomes = [link.get('href') for link in soup.findAll('a') if link.get('href').startswith('UP')]
+    shuffle(proteomes)
+    added = 0
+    i = 0
+    pbar_while = tqdm(desc=f"Downloading {king}", total=count)
+    while added < count:
+        prot = proteomes[i]
+        html_page = r.get(ftp_url + prot).text
+        soup = BeautifulSoup(html_page, 'html.parser')
+        links = [link.get('href') for link in soup.findAll('a') if link.get('href').startswith('UP') if
+                 link.get('href').find('fasta') != -1 and link.get('href').find('DNA.fasta') == -1]
+        if len(links) == 1:
+            added += 1
+        try:
+            wget.download(ftp_url + prot + links[0], path + links[0])
+        except FileNotFoundError:
+            os.mkdir(path)
+            wget.download(ftp_url + prot + links[0], path + links[0])
+
+        i += 1
+        pbar_while.update()
+    return king
+
+
+def download_query(tax_id: str, path: str):
+    rest_link = f"https://rest.uniprot.org/proteomes/stream?query=reference:true+taxonomy_id:{tax_id}&top_node&format=list"
+    proteome_id = r.get(rest_link).text.split()[0]
+    rest_link_prot = f"https://rest.uniprot.org/uniprotkb/stream?query=proteome:{proteome_id}&format=fasta"
+    with r.get(rest_link_prot, stream=True) as req:
+        req.raise_for_status()
+        try:
+            with open(path + tax_id + '.fasta', 'wb') as f:
+                for chunk in req.iter_content(chunk_size=2 ** 20):
+                    f.write(chunk)
+        except FileNotFoundError:
+            os.mkdir(path)
+            with open(path + tax_id + '.fasta', 'wb') as f:
+                for chunk in req.iter_content(chunk_size=2 ** 20):
+                    f.write(chunk)
+    return proteome_id
+
+
+def download_swiss(path: str):
+    url = "https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz"
+    wget.download(url, f"{path}swissprot.fasta.gz")
+
+
+def download_pdb(path: str):
+    url = "https://ftp.wwpdb.org/pub/pdb/derived_data/pdb_seqres.txt.gz"
+    wget.download(url, f"{path}pdb.fasta.gz")
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(prog="download",
+                            description="download proteomes")
+    parser.add_argument('data_path', action='store', help="where should data be sotred")
+    parser.add_argument('--queries', '-q', action='store_true', help="should proteomes of organisms listed in exercise"
+                                                                     " description be downloaded")
+    parser.add_argument('--king', '-k', action='store_true', help="should proteomes of organisms in kingdoms given "
+                                                                  "in exercise description be downloaded")
+    parser.add_argument('--pdb', '-p', action='store_true', help='should pdb be downloaded')
+    parser.add_argument('--swiss', '-s', action='store_true', help='should swissprot be downloaded')
+
+    args = parser.parse_args()
+
+    path = args.data_path
+    os.makedirs(path, exist_ok=True)
+
+    if args.queries:
+        queries = ['9606', '562', "224308", "559292", "3702", "7227", "6239", "10090", "7955"]
+        pbar = tqdm(queries)
+        for query in pbar:
+            pbar.set_description(f"downloaded proteome: {download_query(query, f'{path}/misc/')}")
+
+    if args.king:
+        kingdoms = ['Archaea', "Eukaryota", "Viruses", "Bacteria"]
+        for kingdom in kingdoms:
+            download_kingdom(kingdom, f'{path}/{kingdom}/', 10)
+
+    if args.pdb:
+        download_pdb(f'{path}/databases/')
+
+    if args.swiss:
+        download_swiss(f'{path}/databases/')
---
+++ lab3&4/scripts/plots.py
@@ -0,0 +1,84 @@
+import gzip
+from os import listdir
+from os.path import isfile, join
+from matplotlib import pyplot as plt
+import numpy as np
+from Bio import SeqIO
+from scipy import stats
+from typing import Tuple
+
+
+def get_length_org(mypath: str) -> list:
+    """
+    Calculates lengths of proteins of organisms in given directory
+    :param mypath: path to directory with proteomes
+    :return: list of tuples of form (filename, mean_length, sem)
+    """
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+    orgs = []
+    for file in onlyfiles:
+        lens = []
+        if file.endswith('gz'):
+            with gzip.open(f"{mypath}/{file}", "rt") as handle:
+                for record in SeqIO.parse(handle, "fasta"):
+                    if record.seq:
+                        lens.append(len(record.seq))
+        elif file.endswith('fasta'):
+            print(file)
+            for record in SeqIO.parse(f"{mypath}/{file}", "fasta"):
+                if record.seq:
+                    lens.append(len(record.seq))
+
+        orgs.append((lens, file))
+
+    ret = [(org[0], np.mean(org[0]), stats.sem(org[0]), org[1]) for org in orgs]
+
+    return ret
+
+
+def get_length_king(orgs: list) -> Tuple:
+    """
+    Calculates mean length of protein in given list of organisms
+    :param orgs: list of form as returned by `get_length_org`
+    :return: tuple of mean and sem
+    """
+    ret = [x[0] for x in orgs]
+    ret = [item for sublist in ret for item in sublist]
+
+    return np.mean(ret), stats.sem(ret)
+
+
+def plot_kingdoms(path: str):
+    """
+    Plots mean length of proteins in given directory
+    :param path: path to directory for which mean protein lengths should be calculated
+    :return: Nothing
+    """
+    data = {key: get_length_org(path + key) for key in ['Archaea', "Eukaryota", "Viruses", "Bacteria"]}
+    kings = {key: get_length_king(value) for key, value in data.items()}
+    print([x[1] for x in kings.values()])
+    plt.bar(range(len(kings)), [x[0] for x in kings.values()],
+            yerr=[x[1] for x in kings.values()], tick_label=[key for key, _ in kings.items()])
+    plt.show()
+
+
+def plot_databases(path: str):
+    """
+    Plots mean length of proteins for each file in given directory
+    :param path: path to directory for which mean protein lengths should be calculated
+    :return: Nothing
+    """
+    data = get_length_org(path)
+    print(data[0])
+    plt.bar(range(len(data)), [x[1] for x in data],
+            yerr=[x[2] for x in data], tick_label=[x[3].split('.')[0] for x in data])
+    plt.show()
+
+
+if __name__ == '__main__':
+    path = '../data/'
+    plot_databases(path + 'misc')
+    plot_databases(path + 'databases')
+    plot_kingdoms(path)
+
